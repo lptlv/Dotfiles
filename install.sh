987db35f21334bae197a3e89942700a4e8b1c4a4
lptlv <119885342+lptlv@users.noreply.github.com>
2023-09-12 12:45:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 mv .config/* ~/.config/
 mv .local/bin/sway-new-workspace ~/.local/bin
+
+mkdir ~/.icons
 mv .icons/* ~/.icons
+
+mkdir ~/.themes
 mv .themes/* ~/.themes
+
+mkdir ~/.emacs.d
 mv .emacs.d/* ~/.emacs.d
+
 mv .bashrc ~ 
