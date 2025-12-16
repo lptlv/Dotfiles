70491e89b668bd8f5d8ec6f5e2359fc3d34373f6
lptlv <119885342+lptlv@users.noreply.github.com>
2025-12-16 14:31:53 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,7 +3,11 @@ mkdir ~/Pictures/Screenshots
 mv Wallpapers ~/Pictures/
 cp -r .config/* ~/.config/
 cp -r .icons ~
-mv .zshrc ~
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~
+
+mkdir -p ~/.config/quickshell/noctalia-shell && curl -sL https://github.com/noctalia-dev/noctalia-shell/releases/latest/download/noctalia-latest.tar.gz | tar -xz --strip-components=1 -C ~/.config/quickshell/noctalia-shell
 
 sudo transactional-update run zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
 sudo transactional-update run zypper addrepo https://download.opensuse.org/repositories/home:/astra/openSUSE_Tumbleweed home:astra/Tumbleweed
