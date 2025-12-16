a5f930d5041b6277db606240329e0e2e90f9e222
lptlv <119885342+lptlv@users.noreply.github.com>
2025-12-16 11:28:19 +0100

    Delete .config/river directory

--- .config/river/scripts/sleep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-killall sleep.sh
-
-swayidle -w \
-         timeout 600 'sh /home/lptlv/.config/river/scripts/screenlock.sh' \
-         timeout 1200 'systemctl suspend' \
-         before-sleep 'playerctl pause' \
-         before-sleep 'sh /home/lptlv/.config/river/scripts/screenlock.sh'
