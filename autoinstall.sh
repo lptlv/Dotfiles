bf1c8a67fe1cb17c609b46b51df664ac1d44702d
lptlv <119885342+lptlv@users.noreply.github.com>
2025-12-16 13:00:26 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,3 +4,4 @@ cp -r .icons ~
 mv .zshrc ~
 
 sudo transactional-update run zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
+sudo transactional-update run zypper addrepo https://download.opensuse.org/repositories/home:/astra/openSUSE_Tumbleweed home:astra/Tumbleweed
