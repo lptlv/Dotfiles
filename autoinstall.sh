1ef6d3e4d41e35608517f737ac45d7901dfed9a9
lptlv <119885342+lptlv@users.noreply.github.com>
2025-12-16 14:40:26 +0100

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -11,3 +11,5 @@ mkdir -p ~/.config/quickshell/noctalia-shell && curl -sL https://github.com/noct
 
 sudo transactional-update run zypper ar -cfp 90 'https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/Essentials/' packman-essentials
 sudo transactional-update run zypper addrepo https://download.opensuse.org/repositories/home:/astra/openSUSE_Tumbleweed home:astra/Tumbleweed
+sudo transactional-update -i pkg in quickshell gpu-screen-recorder-gtk cava ddcutil cliphist wlsunset swaylock swayidle sway-contrib xdg-desktop-portal-wlr evolution-data-server polkit-kde-agent-6
+sudo transactional-update apply
